46aa1b114c6db5167e22a80b637107830d3aeebb
Nick Panteleeff <drnixx@gmail.com>
2022-10-19 20:58:54 +0300

    v6 start

---
+++ packages/react/src/components/stencil-generated/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ionic-selectable/core';
+
+export const IonicSelectable = /*@__PURE__*/createReactComponent<JSX.IonicSelectable, HTMLIonicSelectableElement>('ionic-selectable');
